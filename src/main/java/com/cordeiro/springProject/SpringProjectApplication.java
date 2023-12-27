2ed651f119afb56241f2eef99f2bcc97720b45e2
DaniloYuri <daniloyuri.dev@gmail.com>
2023-12-27 15:14:02 -0300

    Criando operação de instanciacao

--- src/main/java/com/cordeiro/springProject/SpringProjectApplication.java
+++ src/main/java/com/cordeiro/springProject/SpringProjectApplication.java
@@ -1,13 +1,32 @@
 package com.cordeiro.springProject;
 
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringProjectApplication {
+import com.cordeiro.springProject.domain.Categoria;
+import com.cordeiro.springProject.repositorys.CategoriaRepository;
 
+
+@SpringBootApplication
+public class SpringProjectApplication implements CommandLineRunner {
+	@Autowired 
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringProjectApplication.class, args);
 	}
 
+	public void run (String...args) throws Exception {
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "enfermagem");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		
+	}
+	
 }
